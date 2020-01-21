f012cbd078c0a2e23e41309d9a94718a8556fd83
Kacper Lachowski <lachowskik27@gmail.com>
2020-01-21 00:50:59 -0800

    Added methods to A1Stock class to convert weight fraction to molarity or volume fraction.

--- ot2protocol/A1Stock.py
+++ ot2protocol/A1Stock.py
@@ -7,7 +7,7 @@ class A1Stock:
     components_dict - dictionary pointing to Component objects: 
     e.g. {'A':Component, 'solvent1':Component}
     
-    wt_f_dict - dictionary which ties components to their respective weight fractions:
+    wtf_dict - dictionary which ties components to their respective weight fractions:
     e.g. {'A':float, 'solvent1':float}
     
     density - float. By default this is solvent1.density
@@ -19,14 +19,39 @@ class A1Stock:
     switch to new stock position when necessary.
     '''
     
-    def __init__(self, components_dict ={}, wt_f_dict ={}, density = None):
+    def __init__(self, components_dict ={}, wtf_dict ={}, density = None):
         self.componentA = components_dict['A']
         self.solvent1 = components_dict['solvent1']
 
-        self.componentA_wtf = wt_f_dict['A']
-        self.solvent1_wtf = wt_f_dict['solvent1']
+        self.componentA_wtf = wtf_dict['A']
+        self.solvent1_wtf = wtf_dict['solvent1']
         
         self.density = self.solvent1.density #Approximate density as density of solvent1 for now. 
+                
+    
+    def wtf_to_volf(self):
+        try:
+            print(self.componentA.density)
+        except AttributeError:
+            print(self.componentA.name, " has no density.")
+        try:
+            print(self.solvent1.density)
+        except AttributeError:
+            print(self.solvent1.name, " has no density.")    
+        self.componentA_volf = (self.componentA_wtf/self.componentA.density)/((self.componentA_wtf/self.componentA.density) + (self.solvent1_wtf/self.solvent1.density))
+        self.solvent1_volf = 1.0 - self.componentA_volf
+    
+    def wtf_to_molarity(self):
+        componentA_moles = self.componentA_wtf/self.componentA.mw
+
+        try:
+            print(self.solvent1.density)
+        except AttributeError:
+            print(self.solvent1.name, " has no density.")   
+        solvent1_placeholder_volume = self.solvent1_wtf/self.solvent1.density
+        
+        self.componentA_molarity = componentA_moles*solvent1_placeholder_volume/1000
+
     def real_init(self):
         pass
 
